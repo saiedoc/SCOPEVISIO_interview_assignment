fb7c36665cb91ac3e659bdf6d99155124a5c4571
Saied Aussi <saiedaussi@uni-bonn.de>
2024-09-12 18:20:34 +0200

    Project Creation & Environment Set up

---
+++ src/main/java/com/interview/scopevisio/ScopevisioApplication.java
@@ -0,0 +1,13 @@
+package com.interview.scopevisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopevisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopevisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/scopevisio/ScopevisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.scopevisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopevisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
